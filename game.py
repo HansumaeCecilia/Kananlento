2002b72e083b005b7abdb10bb7c3a2e4cb041d02
HansumaeCecilia <48033@edu.raseko.fi>
2023-04-03 14:23:19 +0300

    Started game code

---
+++ game.py
@@ -0,0 +1,38 @@
+import pygame
+
+def main():
+    game = Game()
+    game.run()
+
+class Game:
+    def __init__(self):
+        pygame.init() # Initializes modules     
+        self.screen = pygame.display.set_mode((800, 600)) # Screen size, coordinates as tuple within their own parenthesis
+        self.running = False # Game is running
+
+    def run(self):                    
+        clock = pygame.time.Clock()               
+        
+        # While loop for quitting the game
+        while self.running:            
+            self.handle_events()
+            self.handle_game_logic()         
+            self.update_screen()               
+            clock.tick(60) # Wait until screen's update speed is 60fps
+
+        pygame.quit() # Quits the game
+
+    def handle_events(self):
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+    def update_screen(self):
+        self.screen.fill((240, 240, 255)) # Give screen color by text (ie. "green") or rpg  
+        pygame.display.flip()
+
+    def handle_game_logic(self):
+        pass
+
+if __name__ == "__main__":
+    main()
