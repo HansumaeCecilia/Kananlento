c7e3a757fb46377df4a75dd4e5c658b19609f51f
HansumaeCecilia <48033@edu.raseko.fi>
2023-04-03 18:22:00 +0300

    Position, gravity, speed, etc.

--- game.py
+++ game.py
@@ -9,6 +9,17 @@ class Game:
         pygame.init() # Initializes modules     
         self.screen = pygame.display.set_mode((800, 600)) # Screen size, coordinates as tuple within their own parenthesis
         self.running = False # Game is running
+        self.init_graphics()
+        self.init_objects()
+    
+    def init_graphics(self):
+        img_bird1 = pygame.image.load("images/chicken/flying/frame-1.png")
+        self.img_bird1 = pygame.transform.rotozoom(img_bird1, 0, 1/16)
+    
+    def init_objects(self):
+        self.bird_y_speed = 1
+        self.bird_pos = (200, 300)
+        self.bird_lift = False
 
     def run(self):                    
         clock = pygame.time.Clock()               
@@ -27,13 +38,34 @@ class Game:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key in (pygame.K_SPACE, pygame.K_UP):
+                        self.bird_lift = True
+                elif event.type == pygame.KEYUP:
+                    if event.key in (pygame.K_SPACE, pygame.K_UP):
+                        self.bird_lift = False
+    
 
-    def update_screen(self):
-        self.screen.fill((240, 240, 255)) # Give screen color by text (ie. "green") or rpg  
+    def handle_game_logic(self):        
+        bird_y = self.bird_pos[1]
+
+        
+        if self.bird_lift:
+            # Bird gets lifted (0.5 px / frame)
+            self.bird_y_speed -= 0.5
+        else:        
+            # Gravity (adds falling velocity in every frame)
+            self.bird_y_speed += 0.2
+
+        # Move the bird
+        bird_y += self.bird_y_speed
+        self.bird_pos = (self.bird_pos[0], bird_y) # Bird's position for movement
+
+    def update_screen(self):        
+        self.screen.fill((240, 240, 255)) # Give screen color by text (ie. "green") or rpg        
+        self.screen.blit(self.img_bird1, self.bird_pos) # Draw the bird
+
         pygame.display.flip()
-
-    def handle_game_logic(self):
-        pass
 
 if __name__ == "__main__":
     main()
