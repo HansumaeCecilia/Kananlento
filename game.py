6e266241ab7b20ba043b298cf71068dab1f6aaa1
HansumaeCecilia <48033@edu.raseko.fi>
2023-04-17 09:25:26 +0300

    Added font, code for game, obstacle-class, etc.

--- game.py
+++ game.py
@@ -1,4 +1,9 @@
+import random
 import pygame
+
+DEFAULT_SCREEN_SIZE = (800, 450)
+FPS_TEXT_COLOR = (128, 0, 128) # Dark purple
+TEXT_COLOR = (128, 0, 0) # Dark red
 
 def main():
     game = Game()
@@ -7,22 +12,57 @@ def main():
 class Game:
     def __init__(self):
         pygame.init() # Initializes modules     
-        self.screen = pygame.display.set_mode((800, 600)) # Screen size, coordinates as tuple within their own parenthesis
+        self.clock = pygame.time.Clock()
+        self.is_fullscreen = False
+        self.show_fps = True
+        self.screen = pygame.display.set_mode(DEFAULT_SCREEN_SIZE)
+        self.screen_w = self.screen.get_width()
+        self.screen_h = self.screen.get_height()        
         self.running = False # Game is running
+        self.font16 = pygame.font.Font("fonts/SyneMono-Regular.ttf", 16)
         self.init_graphics()
         self.init_objects()
     
     def init_graphics(self):
-        img_bird1 = pygame.image.load("images/chicken/flying/frame-1.png")
-        self.img_bird1 = pygame.transform.rotozoom(img_bird1, 0, 1/16)
-    
-    def init_objects(self):
-        self.bird_y_speed = 1
-        self.bird_pos = (200, 300)
-        self.bird_lift = False
+        big_font_size = int(96 * self.screen_h / 450)
+        self.font_big = pygame.font.Font("fonts/SyneMono-Regular.ttf", big_font_size)
+        original_bird_images = [pygame.image.load(f"images/chicken/flying/frame-{i}.png")
+                                for i in [1, 2, 3, 4]]        
+        
+        self.bird_imgs = [
+            pygame.transform.rotozoom(x, 0, self.screen_h / 9600).convert_alpha()
+            for x in original_bird_images
+        ]     
+        original_bird_dead_images = [
+            pygame.image.load(f"images/chicken/got_hit/frame-{i}.png")
+            for i in [1, 2]
+        ]
 
-    def run(self):                    
-        clock = pygame.time.Clock()               
+        self.bird_dead_imgs = [
+            pygame.transform.rotozoom(img, 0, self.screen_h / 9600).convert_alpha()
+            for img in original_bird_images
+        ]   
+        original_bg_images = [
+            pygame.image.load(f"images/background/layer_{i}.png")
+            for i in [1, 2, 3]
+        ]
+        self.bg_imgs = [
+            pygame.transform.rotozoom(img, 0, self.screen_h / img.get_height()).convert_alpha()
+            for img in original_bg_images
+        ]
+        self.bg_widths = [img.get_width() for img in self.bg_imgs]
+        self.bg_pos = [0, 0, 0]
+
+    def init_objects(self):
+        self.bird_alive = True
+        self.bird_y_speed = 0
+        self.bird_pos = (self.screen_w / 3, self.screen_h / 4)
+        self.bird_angle = 0
+        self.bird_frame = 0
+        self.bird_lift = False
+        self.obstacles = [Obstacle.make_random(self.screen_w, self.screen_h)]
+     
+    def run(self):                                         
         self.running = True
         
         # While loop for quitting the game
@@ -30,7 +70,7 @@ class Game:
             self.handle_events()
             self.handle_game_logic()         
             self.update_screen()               
-            clock.tick(60) # Wait until screen's update speed is 60fps
+            self.clock.tick(60) # Wait until screen's update speed is 60fps
 
         pygame.quit() # Quits the game
 
@@ -44,28 +84,136 @@ class Game:
                 elif event.type == pygame.KEYUP:
                     if event.key in (pygame.K_SPACE, pygame.K_UP):
                         self.bird_lift = False
+                    elif event.key in (pygame.K_f, pygame.K_F11):
+                        self.toggle_fullscreen()
+                    elif event.key in (pygame.K_r, pygame.K_RETURN):
+                        self.init_objects()
     
 
-    def handle_game_logic(self):        
+    def toggle_fullscreen(self):
+        old_w = self.screen_w
+        old_h = self.screen_h
+        if self.is_fullscreen:
+            pygame.display.set_mode(DEFAULT_SCREEN_SIZE)
+            self.is_fullscreen = False
+        else:
+            pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.is_fullscreen = True
+        screen = pygame.display.get_surface()
+        self.screen_w = screen.get_width()
+        self.screen_h = screen.get_height()
+        self.init_graphics()
+        self.scale_positions(
+            scale_x=(self.screen_w / old_w),
+            scale_y=(self.screen_h / old_h),
+        )
+    
+    def handle_game_logic(self):
+        if self.bird_alive:
+            self.bg_pos[0] -= 0.5
+            self.bg_pos[1] -= 1
+            self.bg_pos[2] -= 3
+
         bird_y = self.bird_pos[1]
 
         
-        if self.bird_lift:
+        if self.bird_alive and self.bird_lift:
             # Bird gets lifted (0.5 px / frame)
             self.bird_y_speed -= 0.5
         else:        
             # Gravity (adds falling velocity in every frame)
             self.bird_y_speed += 0.2
+        
+        if self.bird_lift or not self.bird_alive:
+            self.bird_frame += 1
 
-        # Move the bird
-        bird_y += self.bird_y_speed
-        self.bird_pos = (self.bird_pos[0], bird_y) # Bird's position for movement
+        bird_y += self.bird_y_speed        
+
+        if self.bird_alive:
+            self.bird_angle = -90 * 0.04 * self.bird_y_speed
+            self.bird_angle = max(min(self.bird_angle, 60), -60)
+
+        if bird_y > self.screen_h * 0.78:
+            bird_y = self.screen_h * 0.78
+            self.bird_y_speed = 0
+            self.bird_alive = False
+        
+        self.bird_pos = (self.bird_pos[0], bird_y)
+
+        for obstacle in self.obstacles:
+            obstacle.move(self.screen_w * 0.005)
 
     def update_screen(self):        
-        self.screen.fill((240, 240, 255)) # Give screen color by text (ie. "green") or rpg        
-        self.screen.blit(self.img_bird1, self.bird_pos) # Draw the bird
+        for i in range(len(self.bg_imgs)):
+            # Ensin piirrä vasen tausta
+            self.screen.blit(self.bg_imgs[i], (self.bg_pos[i], 0))
+            # Jos vasen tausta ei riitä peittämään koko ruutua, niin...
+            if self.bg_pos[i] + self.bg_widths[i] < self.screen_w:
+                # ...piirrä sama tausta vielä oikealle puolelle
+                self.screen.blit(
+                    self.bg_imgs[i],
+                    (self.bg_pos[i] + self.bg_widths[i], 0)
+                )
+            # Jos taustaa on jo siirretty sen leveyden verran...
+            if self.bg_pos[i] < -self.bg_widths[i]:
+                # ...niin aloita alusta
+                self.bg_pos[i] += self.bg_widths[i]
+
+        for obstacle in self.obstacles:
+            obstacle.render(self.screen)
+
+        # Piirrä lintu
+        if self.bird_alive:
+            bird_img_i = self.bird_imgs[(self.bird_frame // 3) % 4]
+        else:
+            bird_img_i = self.bird_dead_imgs[(self.bird_frame // 10) % 2]
+        bird_img = pygame.transform.rotozoom(bird_img_i, self.bird_angle, 1)
+        self.screen.blit(bird_img, self.bird_pos)
+
+        if not self.bird_alive:
+            game_over_img = self.font_big.render("GAME OVER", True, TEXT_COLOR)
+            x = self.screen_w / 2 - game_over_img.get_width() / 2
+            y = self.screen_h / 2 - game_over_img.get_height() / 2
+            self.screen.blit(game_over_img, (x, y))
+
+        # Piirrä FPS luku
+        if self.show_fps:
+            fps_text = f"{self.clock.get_fps():.1f} fps"
+            fps_img = self.font16.render(fps_text, True, FPS_TEXT_COLOR)
+            self.screen.blit(fps_img, (0, 0))
+
 
         pygame.display.flip()
+
+class Obstacle:
+    def __init__(self, position, upper_height, lower_height, width=100):
+        self.position = position  # vasemman reunan sijainti
+        self.upper_height = upper_height
+        self.lower_height = lower_height
+        self.width = width
+        self.color = (0, 128, 0)  # dark green
+
+    @classmethod
+    def make_random(cls, screen_w, screen_h):
+        h1 = random.randint(int(screen_h * 0.05), int(screen_h * 0.75))
+        h2 = random.randint(int((screen_h - h1) * 0.05),
+                             int((screen_h - h1) * 0.75))
+        return cls(upper_height=h1, lower_height=h2, position=screen_w)
+
+    def move(self, speed):
+        self.position -= speed
+
+    def is_visible(self):
+        return self.position + self.width >= 0
+
+    def render(self, screen):
+        x = self.position
+        uy = 0
+        uh = self.upper_height
+        pygame.draw.rect(screen, self.color, (x, uy, self.width, uh))
+        ly = screen.get_height() - self.lower_height
+        lh = self.lower_height
+        pygame.draw.rect(screen, self.color, (x, ly, self.width, lh))
 
 if __name__ == "__main__":
     main()
