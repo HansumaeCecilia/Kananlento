301cc178c8081a8a2eb3bbcab556f674ff849a21
HansumaeCecilia <48033@edu.raseko.fi>
2023-05-08 13:09:42 +0300

    Added code for highscore recording

--- highscore.py
+++ highscore.py
@@ -19,21 +19,31 @@ class HighscoreRecorder:
         self.color = color
         self.font_file = font_file
         self.set_font_size(font_size)
+        self.text = ""
+        self.score = None
 
     def set_font_size(self, size):
         self.font = pygame.font.Font(self.font_file, size)
 
+    def record_highscore(self, score):
+        self.score = score
+
     def handle_event(self, event):
         if event.type != pygame.KEYUP:
             return None
-        if event.key == pygame.K_ESCAPE:
+        if event.key == pygame.K_ESCAPE:            
             return HighscoreAction.CLOSE
         elif event.key == pygame.K_RETURN:
+            # TODO: Save highscore to file
             return HighscoreAction.CLOSE            
+        elif event.key == pygame.K_BACKSPACE:
+            self.text = self.text[:-1]
 
     def render(self, screen):
         texts_and_colors = [
             ("New highscore!", self.color),
+            (str(self.score), self.color),
             ("Enter your name: ", self.color),
+            (self.text, self.color)
         ]
         render_centered_text_lines(screen, self.font, texts_and_colors)
